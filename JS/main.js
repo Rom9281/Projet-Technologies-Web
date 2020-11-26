3dc936fc6b8989c5edb0bd3510233efd11f89101
Romain GAUD <romaingaudfra@gmail.com>
2020-11-26 16:39:09 +0100

    Definitions des messages d'erreures, mise en place du prix variable

--- JS/main.js
+++ JS/main.js
@@ -36,73 +36,124 @@ function Recap(){
 }
 
 function prntPrix(){
+    getInfo()
     var prix_adulte = getChoice('prix_adulte')
     var prix_enfant = 0.4*prix_adulte
-    var inputs = document.getElementById('form').elements;
-    var petit_dej = document.getElementById("petit_dej").value;
-    if (validDate()){
-        var nb_jours = validDate()
 
-        if ((parseInt(inputs["Nombre_d'adultes"].value) == undefined) && (parseInt(inputs["Nombre_d'enfants"].value) == undefined )){
-            chgbyId('0','prix');
-        }else if (parseInt(inputs["Nombre_d'adultes"].value) == undefined){
-            
-            chgbyId(parseInt(inputs["Nombre_d'enfants"].value)*prix_enfant*nb_jours,'prix');
-    
-        }else if(parseInt(inputs["Nombre_d'enfants"].value) == undefined){
-            var prix = parseInt(inputs["Nombre_d'adultes"].value)*prix_adulte*nb_jours
-            chgbyId(prix,'prix');
-        }
-        else{
-            var prix = (parseInt(inputs["Nombre_d'adultes"].value)*prix_ad+parseInt(inputs["Nombre_d'enfants"].value)*prix_en)*nb_jours;
-            chgbyId(prix,'prix');
-        }
+    var petit_dej = document.getElementById("petit_dej").checked;
+
+    var inputs = document.getElementById('form').elements;
+    var nombre_adultes = parseInt(inputs["Nombre_d'adultes"].value);
+    var nombre_enfants = parseInt(inputs["Nombre_d'enfants"].value);
+
+    if (validDate()){
+        var nb_jours = calcJours()
+        var prix = nombre_adultes*prix_adulte;
+        prix = prix + nombre_enfants*prix_enfant;
+        prix = prix*nb_jours;
+        prix = prix + 14*nb_jours*(nombre_adultes+nombre_enfants)*petit_dej
+        chgbyId(prix,'prix');
     }
-    
-    
+    else{
+        chgbyId('Veuillez selectionner des dates valides pour afficher le prix.','prix');
+    }
 }
 
+function calcJours(){
+    // !!! On suppose que le retour soit apres l'arrivee: toujours mettre donc apres une verification de la date !!!
+    var inputs = document.getElementById('form').elements;
+    var depart = inputs["Date_de_depart"].value;
+    var retour = inputs["Date_de_retour"].value;
+    var depart = new Date(depart); 
+    var retour = new Date(retour); 
+  
+    // Calcul de la difference de temps
+    var diff_temp = retour.getTime() - depart.getTime(); 
+  
+    // To calculate the no. of days between two dates 
+    var diff_jours = diff_temp / (1000 * 3600 * 24);
+    
+    return diff_jours
+}
+
+
+//---------- Validation des criteres --------
 function validDate(){
     var inputs = document.getElementById('form').elements;
     var depart = inputs["Date_de_depart"].value;
     var retour = inputs["Date_de_retour"].value;
     if(depart < retour){
-        return retour-depart
+        return true
     }
     else{
-        return False
+        return false
+    }
+}
+function validClient(){
+    var inputs = document.getElementById('form').elements;
+    var nombre_adultes = inputs["Nombre_d'adultes"].value;
+    if(nombre_adultes >= 1){
+        return true
+    }
+    else{
+        return false
     }
 }
 
+//--------------------------------------------
+//--------  Erreures possibles ---------------
 function date_error(){
-    if (!(document.getElementById("erreur") == null)){
-        document.getElementById("erreur").remove();
+    if (!(document.getElementById("date_error") == null)){
+        document.getElementById("date_error").remove();
     }
-    
     var div1 = document.createElement("div");
-    div1.setAttribute('id',"erreur");
-    var contenu1 = document.createTextNode(" - !Erreur! : Verifiez bien que le depart soit avant le retour, ou que la date est valide");
+    div1.setAttribute('id',"date_error");
+    div1.setAttribute('class',"error")
+    var contenu1 = document.createTextNode("!Erreur! : Le depart est avant ou egal au retour, ou la date n'est pas valide");
 
     // ajoute le nœud texte au nouveau div créé
     div1.appendChild(contenu1);
 
     // ajoute le nouvel élément créé et son contenu dans le DOM
-    var currentDiv1 = document.getElementById('erreur_date');
+    var currentDiv1 = document.getElementById('error');
     document.body.insertBefore(div1, currentDiv1);
 }
 
+function client_error(){
+    if (!(document.getElementById("client_error") == null)){
+        document.getElementById("client_error").remove();
+    }
+    var div1 = document.createElement("div");
+    div1.setAttribute('id',"client_error");
+    div1.setAttribute('class',"error")
+    var contenu1 = document.createTextNode("!Erreur! : Il n'y a aucun passager, ou il n'y a que des enfants, ce qui est interdit. ");
+
+    // ajoute le nœud texte au nouveau div créé
+    div1.appendChild(contenu1);
+
+    // ajoute le nouvel élément créé et son contenu dans le DOM
+    var currentDiv1 = document.getElementById('error');
+    document.body.insertBefore(div1, currentDiv1);
+}
+
+
+//------------------------------------------------
+//------ Verifiction pour passer au Recap---------
 function saveF(){
     if (validDate()){
-        document.forms["form"].submit;
-        window.location.replace("Recapitulatif.html");
+        if(validClient()){
+            document.forms["form"].submit;
+            window.location.replace("Recapitulatif.html");
+        }
+        else{
+            client_error()
+        }
     }
     else{
         date_error()
     }
 
 }
-
-
 
 function init(){
     if (getChoice("Init") == null){
@@ -153,8 +204,8 @@ function getInfo(){
     })
     .then(function(json) {
         for (var i = 0; i < json.length;i++) {
-            if (json[i].destination == destination){
-                var prix_adulte = json[i].prixAd;
+            if (json[i].Nom == destination){
+                var prix_adulte = json[i].PrixAd;
                 saveChoice("prix_adulte", prix_adulte)
             }
         }
